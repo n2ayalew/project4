7807e05834ec4086268ac9a0aad351821f614a55
gbleaney <gbleaney@gmail.com>
2016-12-04 22:28:26 -0500

    ADC gravity changing working

--- project4-master/ADC.h
+++ project4-master/ADC.h
@@ -8,11 +8,13 @@
 #ifndef __ADC_H
 #define __ADC_H
  
-extern unsigned int ADCStat;
-extern unsigned int ADCValue;
+extern uint16_t AD_last;
+extern uint8_t  AD_done;
  
 extern void ADC_Init (void);
 extern void ADC_IRQHandler( void );
-extern void ADC_ConversionStart (void );
- 
+extern void ADC_StartCnv (void );
+extern void     ADC_StopCnv (void);
+extern uint16_t ADC_GetCnv  (void);
+
 #endif
